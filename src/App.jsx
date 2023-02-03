89c9d4cddb14d5e78b2360c1eef63aaf9b61b604
SalmanDoria <salmandoria@gmail.com>
2023-02-03 10:26:34 -0500

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Offers from './pages/Offers'
+import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1 className = 'text-2xl bg-red-400'>hello world</h1>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+
+     <>
+            <Router>
+                 <Routes>
+                     <Route path='/' element={<Home/>}/>
+                     <Route path='/profile' element={<Profile/>}/>
+                     <Route path='/sign-in' element={<SignIn/>}/>
+                     <Route path='/sign-up' element={<SignUp/>}/>
+                     <Route path='/offers' element={<Offers/>}/>
+                     <Route path='/forgot-password' element={<ForgotPassword/>}/>
+                 </Routes>
+         </Router>
+        </>
+    // <div className="App">
+    //   <h1 className = 'text-2xl bg-red-400'>hello world</h1>\
+    //   <h1 className="text-3xl font-bold underline">
+    //     Hello world!
+    //   </h1>
       
 
-    </div>
+    // </div>
   )
 }
 
